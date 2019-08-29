5689fc2125bfcf136d944b7dc9faf7fc39a6289a
Haider <konnect2haider@gmail.com>
2019-08-29 15:43:51 +0500

    New Commit with new rails

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
+
+$(document).on('turbolinks:load',function(){
+    $('.ui.dropdown').dropdown();
+})
